9a694cc76fe55a9657642993105f60b42da672d6
atul <atul@github.com>
2018-12-24 17:55:03 +0530

    add new class test  message

--- src/com/ak/learning/codechef/PlayPiano.java
+++ src/com/ak/learning/codechef/PlayPiano.java
@@ -1,10 +1,34 @@
 package com.ak.learning.codechef;
 
+import java.util.Scanner;
+
 public class PlayPiano {
     public  static  void main(String args[])
     {
-        System.out.println("PlayPiano");
+        Scanner read= new Scanner(System.in);
+        String input= read.nextLine();
+        boolean isvalid = isvalidInput(input);
 
+        if (isvalid)
+        {
+            System.out.println("PlayPiano is proper way");
+        }
+        else
+        {
+            System.out.println("PlayPiano is  not proper way");
+        }
+    }
+
+    private static boolean isvalidInput(String input) {
+        if(input.length()%2!=0)
+        {
+            return false;
+        }
+            for (int i = 0; i < input.length(); i += 2) {
+                if (input.charAt(i) == input.charAt(i + 1))
+                   return  false;
+                    }
+            return  true;
     }
 
 }
