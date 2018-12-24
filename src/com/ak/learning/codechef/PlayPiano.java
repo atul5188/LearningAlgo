6be72547aec1a691ecf6d806eec5f2af19a7e59d
atul <atul@github.com>
2018-12-24 17:09:46 +0530

    add new class test  message

--- src/com/ak/learning/codechef/PlayPiano.java
+++ src/com/ak/learning/codechef/PlayPiano.java
@@ -1,7 +1,7 @@
 package com.ak.learning.codechef;
 
 public class PlayPiano {
-    public  static  void main(String arg[])
+    public  static  void main(String args[])
     {
         System.out.println("PlayPiano");
 
